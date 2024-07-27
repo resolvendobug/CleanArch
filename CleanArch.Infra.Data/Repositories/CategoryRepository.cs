208013792f1a0b297ec64e67f8fe52073797351b
Renato Carvalho <renatocp30@gmail.com>
2024-07-27 11:41:52 -0300

    feat: Add CategoryRepository implementation
    This commit adds the implementation of the `CategoryRepository` class in the `CleanArch.Infra.Data.Repositories` namespace. The `CategoryRepository` class implements the `ICategoryRepository` interface and provides methods for creating, retrieving, updating, and removing categories from the database using Entity Framework Core.
    
    The `CategoryRepository` class includes the following methods:
    - `CreateAsync`: Adds a new category to the database.
    - `GetByIdAsync`: Retrieves a category by its ID.
    - `GetCategoriesAsync`: Retrieves all categories from the database.
    - `RemoveAsync`: Removes a category from the database.
    - `UpdateAsync`: Updates a category in the database.
    
    These changes are necessary to support the functionality related to categories in the application.

---
+++ CleanArch.Infra.Data/Repositories/CategoryRepository.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CleanArch.Domain.Entities;
+using CleanArch.Domain.Interfaces;
+using CleanArch.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArch.Infra.Data.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        ApplicationDbContext _categoryContext;
+        public CategoryRepository(ApplicationDbContext categoryContext)
+        {
+            _categoryContext = categoryContext;
+        }
+
+        public async Task<Category> CreateAsync(Category category)
+        {
+            _categoryContext.Add(category);
+            await _categoryContext.SaveChangesAsync();
+            return category;
+        }
+
+        public async Task<Category> GetByIdAsync(int? id)
+        {
+            return await _categoryContext.Categories.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<Category>> GetCategoriesAsync()
+        {
+            return await _categoryContext.Categories.ToListAsync();
+        }
+
+        public async Task<Category> RemoveAsync(Category category)
+        {
+            _categoryContext.Remove(category);
+            await _categoryContext.SaveChangesAsync();
+            return category;
+        }
+
+        public async Task<Category> UpdateAsync(Category category)
+        {
+            _categoryContext.Update(category);
+            await _categoryContext.SaveChangesAsync();
+            return category;
+        }
+    }
+}
