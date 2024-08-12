a033273a9359312ba2ecf43388444dde4ab899aa
Renato Carvalho <renatocp30@gmail.com>
2024-08-12 08:45:31 -0300

    feat: Add MediatR package and create product-related commands and queries

---
+++ CleanArch.Application/Products/Commands/ProductRemoveCommand.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CleanArch.Domain.Entities;
+using MediatR;
+
+namespace CleanArch.Application.Products.Commands
+{
+    public class ProductRemoveCommand : IRequest<Product>
+    {
+        public int Id { get; set; }
+        public ProductRemoveCommand(int id)
+        {
+            Id = id;
+        }
+    }
+}
